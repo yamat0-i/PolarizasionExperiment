0416236e53cee5ac0dd8f1a5a3184618152fd23f
yamat0-i <yamatoiida_61@outlook.jp>
2023-04-28 14:09:25 +0900

    Update: Plots

--- python/20230424/PlotStokes3D.py
+++ python/20230424/PlotStokes3D.py
@@ -23,18 +23,18 @@ fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
 ax.plot(s1_5x, s2_5x, s3_5x, marker='.', color='green', label='5x')
-ax.scatter(-0.78, 0.55, 0.31, marker='s', s=20,  color='green') # Step10
-ax.scatter(-0.77, -0.51, 0.39, marker='v', s=30,  color='green') # Step2
+ax.scatter(D1[0,0], D1[0,1], D1[0,2], marker='s', s=30,  color='green') # Step10
+ax.scatter(D1[8,0], D1[8,1], D1[8,2], marker='v', s=40,  color='green') # Step2
 
 ax.plot(s1_10x, s2_10x, s3_10x, marker='.', color='blue', label='10x')
-ax.scatter(-0.76, -0.48, -0.44, marker='s', s=20,  color='blue') # Step10
-ax.scatter(-0.66, -0.72, -0.24, marker='v', s=30,  color='blue') # Step2
+ax.scatter(D2[0,0], D2[0,1], D2[0,2], marker='s', s=30,  color='blue') # Step10
+ax.scatter(D2[8,0], D2[8,1], D2[8,2], marker='v', s=40,  color='blue') # Step2
 
 ax.plot(s1_40x, s2_40x, s3_40x, marker='.',color='red', label='40x')
-ax.scatter(-0.59, -0.64, 0.49, marker='s', s=20,  color='red') # Step10
-ax.scatter(-0.64, -0.74, -0.23, marker='v', s=30,  color='red') # Step2
+ax.scatter(D3[0,0], D3[0,1], D3[0,2], marker='s', s=30,  color='red') # Step10
+ax.scatter(D3[8,0], D3[8,1], D3[8,2], marker='v', s=40,  color='red') # Step2
 
-ax.set_title('20230424')
+ax.set_title('20230424(■:step10, ▲:step2)')
 
 ax.set_xlabel('s1')
 ax.set_ylabel('s2')
--- python/20230425/PlotStokes3D.py
+++ python/20230425/PlotStokes3D.py
@@ -23,18 +23,18 @@ fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
 ax.plot(s1_5x, s2_5x, s3_5x, marker='.', color='green', label='5x')
-ax.scatter(-0.28, 0.86, 0.42, marker='s', s=20,  color='green') # Step10
-ax.scatter(0.01, 0.96, 0.27, marker='v', s=30,  color='green') # Step2
+ax.scatter(D1[0,0], D1[0,1], D1[0,2], marker='s', s=30,  color='green') # Step10
+ax.scatter(D1[8,0], D1[8,1], D1[8,2], marker='v', s=40,  color='green') # Step2
 
 ax.plot(s1_10x, s2_10x, s3_10x, marker='.', color='blue', label='10x')
-ax.scatter(0.87, 0.49, 0.07, marker='s', s=20,  color='blue') # Step10
-ax.scatter(-0.12, 0.70, 0.70, marker='v', s=30,  color='blue') # Step2
+ax.scatter(D2[0,0], D2[0,1], D2[0,2], marker='s', s=30,  color='blue') # Step10
+ax.scatter(D2[8,0], D2[8,1], D2[8,2], marker='v', s=40,  color='blue') # Step2
 
 ax.plot(s1_40x, s2_40x, s3_40x, marker='.',color='red', label='40x')
-ax.scatter(0.15, 0.27, 0.95, marker='s', s=20,  color='red') # Step10
-ax.scatter(-0.57, 0.18, 0.80, marker='v', s=30,  color='red') # Step2
+ax.scatter(D3[0,0], D3[0,1], D3[0,2], marker='s', s=30,  color='red') # Step10
+ax.scatter(D3[8,0], D3[8,1], D3[8,2], marker='v', s=40,  color='red') # Step2
 
-ax.set_title('20230425')
+ax.set_title('20230425(■:step10, ▲:step2)')
 
 ax.set_xlabel('s1')
 ax.set_ylabel('s2')
