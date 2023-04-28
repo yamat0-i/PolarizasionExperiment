9d47b227d6d4506a4ed821eab91812f3e72ff241
yamat0-i <yamatoiida_61@outlook.jp>
2023-04-28 17:07:57 +0900

    Update

--- python/main.py
+++ python/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 from package.Plot3D import Plot3D
@@ -6,10 +5,6 @@ from package.Plot3D import Plot3D
 #Clean Up
 plt.close('all')
 
-PE20220426_5x = Plot3D("20230426/20230426_5x.txt")
-PE20220426_10x = Plot3D("20230426/20230426_10x.txt")
-PE20220426_40x = Plot3D("20230426/20230426_40x.txt")
-
-PE20220426_5x.load_and_plot(1)
-PE20220426_10x.load_and_plot(1)
-PE20220426_40x.load_and_plot(1)
+PE20230426 = Plot3D("20230426")
+PE20230426.load_and_plot(1)
+PE20230426.load_and_plot_PS(2)
