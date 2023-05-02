76495a78b8e4ac32459b7e0722db5aa1c43e6b99
yamat0-i <yamatoiida_61@outlook.jp>
2023-05-02 14:15:22 +0900

    Update

--- python/AnalyzePol.py
+++ python/AnalyzePol.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 
+from package.FiberLength import measure_length
 from package.Plot3D import Plot3D
 
 #Clean Up
 plt.close('all')
 
-PE20230424 = Plot3D("20230424")
-PE20230424.load_and_plot(1)
-PE20230424.load_and_plot_PS(2)
+FL20230502 = measure_length("20230502")
+FL20230502.measure()
 
-PE20230425 = Plot3D("20230425")
-PE20230425.load_and_plot(3)
-PE20230425.load_and_plot_PS(4)
+# PE20230424 = Plot3D("20230424")
+# PE20230424.load_and_plot(1)
+# PE20230424.load_and_plot_PS(2)
 
-PE20230426 = Plot3D("20230426")
-PE20230426.load_and_plot(5)
-PE20230426.load_and_plot_PS(6)
+# PE20230425 = Plot3D("20230425")
+# PE20230425.load_and_plot(3)
+# PE20230425.load_and_plot_PS(4)
+
+# PE20230426 = Plot3D("20230426")
+# PE20230426.load_and_plot(5)
+# PE20230426.load_and_plot_PS(6)
--- python/package/FiberLength.py
+++ python/package/FiberLength.py
@@ -1,13 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-D = np.loadtxt('data\\{}\\FiberLength_{}'.format(self.date, self.date), delimiter=',',  skiprows=1)
 
-x = D[:,0]
-y1 = D[:.1]
-y2 = D[:,2]
+class measure_length(object):
+    def __init__(self, date):
+        self.date = date
 
-y = y2 - y1
+    def measure(self):
+        D = np.loadtxt('data\\{}\\FiberLength_{}.txt'.format(self.date, self.date), delimiter=',',  skiprows=3)
 
-plt.figure()
-plt.plot(x, y)
+        X = D[:,0]
+        y1 = D[:,1]
+        y2 = D[:,2]
+
+        diameter = y2 - y1
+
+        diameter_min = np.nanmin(diameter)
+        diameter_min_index = np.argmin(diameter)
+        X_min = X[diameter_min_index]
+        print('X:', X_min, ',', 'diameter:', diameter_min)
+
+        plt.figure()
+        plt.plot(X, diameter)
+
+        plt.xlabel('X absolute position')
+        plt.ylabel('diameter[px]')
+        plt.title('{}_FiberDiameter'.format(self.date))
+
+        plt.show()
