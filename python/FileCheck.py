9f5cbf58846cc8b6327c3e6978496812e2ff1c42
yamat0-i <yamatoiida_61@outlook.jp>
2023-05-03 16:41:27 +0900

    Update

--- python/FileCheck.py
+++ python/FileCheck.py
@@ -18,7 +18,7 @@ def file_check():
         pass
     else:
         os.makedirs(date_dir)
-        print("Created: dir '{}'".format(date_dir))
+        print("Create: dir '{}'".format(date_dir))
 
     # create files
     if is_FD_data == True:
@@ -28,7 +28,7 @@ def file_check():
         with open(fname, 'w') as f:
             FD_format = """# Fiber Diameter\n# X: Absolute position, y1:Upper edge, y2: Lower edge\n# X, y1, y2\n"""
             f.write(FD_format)
-        print("Created: {}".format(fname))
+        print("Create: {}".format(fname))
 
     if is_PE_5x_data == True:
         pass
@@ -37,7 +37,7 @@ def file_check():
         with open(fname, 'w', newline='') as f:
             PE_5x_format = """# Polarization Experiment\n# Step 10 -> 2, lens:5x\n# s1 s2 s3\n"""
             f.write(PE_5x_format)
-        print("Created: {}".format(fname))
+        print("Create: {}".format(fname))
 
     if is_PE_10x_data == True:
         pass
@@ -46,7 +46,7 @@ def file_check():
         with open(fname, 'w', newline='') as f:
             PE_10x_format = """# Polarization Experiment\n# Step 10 -> 2, lens:10x\n# s1 s2 s3\n"""
             f.write(PE_10x_format)
-        print("Created: {}".format(fname))
+        print("Create: {}".format(fname))
     
     if is_PE_40x_data == True:
         pass
@@ -55,7 +55,7 @@ def file_check():
         with open(fname, 'w', newline='') as f:
             PE_40x_format = """# Polarization Experiment\n# Step 10 -> 2, lens:40x\n# s1 s2 s3\n"""
             f.write(PE_40x_format)
-        print("Created: {}".format(fname))
+        print("Create: {}".format(fname))
     
     print(
         """Creating format files has been completed.\n
