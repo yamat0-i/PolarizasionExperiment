70a1d1ed77b9ec0dbf1478035b7756018598a04b
yamat0-i <yamatoiida_61@outlook.jp>
2023-05-03 17:12:39 +0900

    Clean

--- python/FileCheck.py
+++ python/FileCheck.py
@@ -18,7 +18,7 @@ def file_check():
         pass
     else:
         os.makedirs(date_dir)
-        print("Create: dir '{}'".format(date_dir))
+        print("Create: '{}'".format(date_dir))
 
     # create files
     if is_FD_data == True:
@@ -26,7 +26,11 @@ def file_check():
     else:
         fname = "{}\\FiberDiameter_{}.txt".format(date_dir, date)
         with open(fname, 'w') as f:
-            FD_format = """# Fiber Diameter\n# X: Absolute position, y1:Upper edge, y2: Lower edge\n# X, y1, y2\n"""
+            FD_format = (
+                "# Fiber Diameter\n"
+                "# X: Absolute position, y1:Upper edge, y2: Lower edge\n"
+                "# X, y1, y2"
+                )
             f.write(FD_format)
         print("Create: {}".format(fname))
 
@@ -35,7 +39,11 @@ def file_check():
     else:
         fname = "{}\\{}_5x.txt".format(date_dir, date)
         with open(fname, 'w', newline='') as f:
-            PE_5x_format = """# Polarization Experiment\n# Step 10 -> 2, lens:5x\n# s1 s2 s3\n"""
+            PE_5x_format = (
+                "# Polarization Experiment\n"
+                "# Step 10 -> 2, lens:5x\n"
+                "# s1 s2 s3\n"
+                )
             f.write(PE_5x_format)
         print("Create: {}".format(fname))
 
@@ -44,7 +52,11 @@ def file_check():
     else:
         fname = "{}\\{}_10x.txt".format(date_dir, date)
         with open(fname, 'w', newline='') as f:
-            PE_10x_format = """# Polarization Experiment\n# Step 10 -> 2, lens:10x\n# s1 s2 s3\n"""
+            PE_10x_format = (
+                "# Polarization Experiment\n"
+                "# Step 10 -> 2, lens:10x\n"
+                "# s1 s2 s3\n"
+                )
             f.write(PE_10x_format)
         print("Create: {}".format(fname))
     
@@ -53,14 +65,17 @@ def file_check():
     else:
         fname = "{}\\{}_40x.txt".format(date_dir, date)
         with open(fname, 'w', newline='') as f:
-            PE_40x_format = """# Polarization Experiment\n# Step 10 -> 2, lens:40x\n# s1 s2 s3\n"""
+            PE_40x_format = (
+                "# Polarization Experiment\n"
+                "# Step 10 -> 2, lens:40x\n"
+                "# s1 s2 s3\n"
+                )
             f.write(PE_40x_format)
         print("Create: {}".format(fname))
     
     print(
-        """Creating format files has been completed.\n
-        Input data.
-        """
+        "Creating files has been completed.\n"
+        "Input data."
         )
 
 
