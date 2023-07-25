b8725e5d4585e2681ae106c6545ce8d6709b1536
yamat0-i <yamatoiida_61@outlook.jp>
2023-07-25 17:19:43 +0900

    Update: S-param plot

--- python/analyzer/models/s_parameter.py
+++ python/analyzer/models/s_parameter.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def main():
     date = _input_date()    
     date_dir_path = get_date_dir_path(date)
-    load_and_plot(date_dir_path, date)
+    load_and_plot_Sparam(date_dir_path, date)
     load_and_plot_PS(date_dir_path, date)
 
 def _input_date():
@@ -41,9 +41,10 @@ def get_date_dir_path(date):
     date_dir_path = "{}\\{}".format(DATA_DIR_PATH, date)
     return date_dir_path
 
-def load_and_plot(date_dir_path, date):
-    """Plot S-parameters in 3D.
+def load_and_plot_Sparam(date_dir_path, date):
+    """Plot S-parameters.
     """
+    # Load data
     D1 = np.loadtxt(
         '{}\\{}_5x.txt'.format(date_dir_path, date), 
         delimiter=',',  skiprows=3
@@ -56,7 +57,8 @@ def load_and_plot(date_dir_path, date):
         '{}\\{}_40x.txt'.format(date_dir_path, date), 
         delimiter=',',  skiprows=3
         )
-
+    
+    # ndarray set
     s1_5x = D1[:,0]
     s2_5x = D1[:,1]
     s3_5x = D1[:,2]
@@ -69,28 +71,37 @@ def load_and_plot(date_dir_path, date):
     s2_40x = D3[:,1]
     s3_40x = D3[:,2]
 
+    step = np.arange(10, 1, -1)
+
+    # Plot
     fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
 
-    ax.plot(s1_5x, s2_5x, s3_5x, marker='.', color='green', label='5x')
-    ax.scatter(D1[0,0], D1[0,1], D1[0,2], marker='s', s=30,  color='green') # Step10
-    ax.scatter(D1[8,0], D1[8,1], D1[8,2], marker='v', s=40,  color='green') # Step2
+    ax1 = fig.add_subplot(3, 1, 1)
+    ax1.invert_xaxis()
+    ax1.plot(step, s1_5x, color='green', label='5x')
+    ax1.plot(step, s1_10x, color='blue', label='10x')
+    ax1.plot(step, s1_40x, color='red', label='40x')
+    ax1.set_xlabel('step')
+    ax1.set_ylabel('s1')
+    ax1.legend(loc=(0.1, 0.1))
 
-    ax.plot(s1_10x, s2_10x, s3_10x, marker='.', color='blue', label='10x')
-    ax.scatter(D2[0,0], D2[0,1], D2[0,2], marker='s', s=30,  color='blue') # Step10
-    ax.scatter(D2[8,0], D2[8,1], D2[8,2], marker='v', s=40,  color='blue') # Step2
+    ax2 = fig.add_subplot(3, 1, 2)
+    ax2.invert_xaxis()
+    ax2.plot(step, s2_5x, color='green', label='5x')
+    ax2.plot(step, s2_10x, color='blue', label='10x')
+    ax2.plot(step, s2_40x, color='red', label='40x')
+    ax2.set_xlabel('step')
+    ax2.set_ylabel('s2')
+    ax2.legend(loc=(0.1, 0.1))
 
-    ax.plot(s1_40x, s2_40x, s3_40x, marker='.',color='red', label='40x')
-    ax.scatter(D3[0,0], D3[0,1], D3[0,2], marker='s', s=30,  color='red') # Step10
-    ax.scatter(D3[8,0], D3[8,1], D3[8,2], marker='v', s=40,  color='red') # Step2
-
-    ax.set_title('{}(■:step10, ▲:step2)'.format(date))
-
-    ax.set_xlabel('s1')
-    ax.set_ylabel('s2')
-    ax.set_zlabel('s3')
-
-    ax.legend()
+    ax3 = fig.add_subplot(3, 1, 3)
+    ax3.invert_xaxis()
+    ax3.plot(step, s3_5x, color='green', label='5x')
+    ax3.plot(step, s3_10x, color='blue', label='10x')
+    ax3.plot(step, s3_40x, color='red', label='40x')
+    ax3.set_xlabel('step')
+    ax3.set_ylabel('s3')
+    ax3.legend(loc=(0.1, 0.1))
 
     plt.show()
 
@@ -98,7 +109,6 @@ def load_and_plot_PS(date_dir_path, date):
     """Plot S-parameters on Poincare sphere.
     """
     # Poincare sphere
-
     r = 1
     theta_1_0 = np.linspace(0, 2*np.pi, 400)
     theta_2_0 = np.linspace(0, 2*np.pi, 400)
@@ -174,7 +184,7 @@ def load_and_plot_PS(date_dir_path, date):
     ax.scatter(D3[0,0], D3[0,1], D3[0,2], marker='s', s=30,  color='red') # Step10
     ax.scatter(D3[8,0], D3[8,1], D3[8,2], marker='v', s=40,  color='red') # Step2
 
-    ax.set_title('{}(■:step10, ▲:step2)'.format('40x'))
+    ax.set_title('{}(■:step10, ▲:step2)'.format(date))
 
     ax.set_xlabel('s1')
     ax.set_ylabel('s2')
